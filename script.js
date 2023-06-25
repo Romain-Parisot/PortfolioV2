2b4627a18c5f34422a4cf47a9a1a51b2ea6c21a1
unknown <romainparisot.pro@gmail.com>
2023-06-25 15:54:43 +0200

    nav bar animation scroll

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function toggleburger(){
     })
 }
 toggleburger();
+
+// -------------------------------------- Nav bar ------------------------------
+
+const navLinks = document.querySelectorAll('.nav-links li');
+const nav_txt = document.querySelectorAll('.nav-links li a');
+console.log(nav_txt);
+console.log(navLinks);
+
+function Sectionvisiblescroll(){
+
+    let sections = document.querySelectorAll('section');
+
+    sections.forEach((section, index) => {
+    // Obtenez la position de chaque section par rapport au haut de la page
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    // Vérifiez si la section est visible à l'écran
+    if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+      // Ajoutez une classe "active" à l'élément de la barre de navigation correspondant à la section visible
+        
+        nav_txt.forEach((nav_txt) => {
+            nav_txt.classList.remove('nav_selected_txt');
+        });
+        nav_txt[index].classList.add('nav_selected_txt');
+        
+        navLinks.forEach((navLink) => {
+            navLink.classList.remove('nav_selected');
+        });
+        navLinks[index].classList.add('nav_selected');
+    }
+});
+
+}
+window.addEventListener('scroll', Sectionvisiblescroll);
