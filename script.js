bdc4e15b20acd97a105c0810cc3483649e5dec77
unknown <romainparisot.pro@gmail.com>
2023-06-25 13:44:32 +0200

    nav bar + burger menu

---
+++ script.js
@@ -0,0 +1,16 @@
+// --------------------------------------burger menu ------------------------------
+function toggleburger(){
+    let header=document.querySelector('header')
+    let burger=document.querySelector('.burger')
+    let barUp=document.querySelector('.bar_up')
+    let barDown=document.querySelector('.bar_down')
+    let barMid=document.querySelector('.bar_mid')
+    burger.addEventListener('click', ()=>{
+        barMid.classList.toggle('hidden');
+        barUp.classList.toggle('barAnimationTop');
+        barDown.classList.toggle('barAnimationBot');
+        header.classList.toggle('animationHeader');
+
+    })
+}
+toggleburger();
