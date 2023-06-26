9e7612371d0cae6f35357414aec966934d0a59da
PARISOT Romain <romainparisot.pro@gmail.com>
2023-06-26 17:01:45 +0200

    Wip logo anim on click

--- script.js
+++ script.js
@@ -59,10 +59,6 @@ let path2All = document.querySelectorAll(".path2");
 
 function logoAnim(){
 
-    let logoSvg1 = document.querySelector(".logo_svg1");
-    let logoSvg2 = document.querySelector(".logo_svg2");
-    let logoSvg3 = document.querySelector(".logo_svg3");
-
     setTimeout(() => {
         path2All.forEach(path2 => {
             path2.classList.add('path2_anim');
@@ -85,11 +81,38 @@ function logoAnim(){
             logoSvg.classList.add('logo_svg');
         });
     }, 8000);
-    logoSvg1.classList.add('logo_svg2');
-    logoSvg2.classList.add('logo_svg3');
-    logoSvg3.classList.add('logo_svg1');
+
 }
 document.addEventListener('DOMContentLoaded', logoAnim);
 document.addEventListener('DOMContentLoaded', () => {
     setInterval(logoAnim, 8000);
+});
+
+let logo_svgAll = document.querySelectorAll(".logo_svg");
+
+function logoAnimClick(){
+    
+    let logoSvg1 = document.querySelector(".logo_svg1");
+    let logoSvg2 = document.querySelector(".logo_svg2");
+    let logoSvg3 = document.querySelector(".logo_svg3");
+
+    
+    logoSvg1.classList.remove('logo_svg1');
+    logoSvg1.classList.remove('logo_svg2');
+    logoSvg1.classList.remove('logo_svg3');
+    logoSvg2.classList.remove('logo_svg1');
+    logoSvg2.classList.remove('logo_svg2');
+    logoSvg2.classList.remove('logo_svg3');
+    logoSvg3.classList.remove('logo_svg1');
+    logoSvg3.classList.remove('logo_svg2');
+    logoSvg3.classList.remove('logo_svg3');
+
+    logoSvg1.classList.add('logo_svg2');
+    logoSvg2.classList.add('logo_svg3');
+    logoSvg3.classList.add('logo_svg1');
+
+}
+
+logo_svgAll.forEach(logo_svg => {
+    logo_svg.addEventListener('click', logoAnimClick);
 });
