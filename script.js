37a4116732b6e5febad55eea87b2b24301a8ab39
PARISOT Romain <romainparisot.pro@gmail.com>
2023-07-20 07:38:08 +0200

    Fixed anim hover

--- script.js
+++ script.js
@@ -661,7 +661,7 @@ function SliderMouseover(slidercont) {
 }
 
 slider_container.forEach(slidercont => {
-    slidercont.addEventListener('mouseover', function(){
+    slidercont.addEventListener('mouseleave', function(){
         SliderMouseover(slidercont)
     })
 });
