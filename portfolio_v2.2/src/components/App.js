51a77080cdd80ddd330f3b6d7f303aec2629c27c
unknown <romainparisot.pro@gmail.com>
2023-10-01 14:55:57 +0200

    script1/2 dark mode

--- portfolio_v2.2/src/components/App.js
+++ portfolio_v2.2/src/components/App.js
@@ -4,6 +4,7 @@ import './App.module.css';
 import Logoanimation from './LogoAnimation/Logoanimation';
 import Burgermenu from './BurgerMenu/Burgermenu';
 import Header from './Header/Header';
+import Darkmode from './DarkMode/Darkmode';
 
 export default function App() {
   const [isBurgerOpen, setisBurgerOpen] = useState(false);
@@ -16,6 +17,7 @@ export default function App() {
       <Logoanimation />
       <Burgermenu onToggleburger={toggleburger} isBurgerOpen={isBurgerOpen} />
       <Header isBurgerOpen={isBurgerOpen} />
+      <Darkmode />
     </div>
   );
 }
