52b8ed6af79cb4f977161263698c3c3e610ae9fd
unknown <romainparisot.pro@gmail.com>
2023-10-01 12:37:12 +0200

    fix animation opening

--- portfolio_v2.2/src/components/App.js
+++ portfolio_v2.2/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Onloadanimation from './OnLoadAnimation/Onloadanimation';
 import './App.module.css';
+// import Logoanimation from './LogoAnimation/Logoanimation';
 
 export default function App() {
   return (
     <div className="App">
       <Onloadanimation />
+      {/* <Logoanimation /> */}
     </div>
   );
 }
--- portfolio_v2.2/src/components/OnLoadAnimation/Onloadanimation.js
+++ portfolio_v2.2/src/components/OnLoadAnimation/Onloadanimation.js
@@ -1,36 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Onloadanimation.module.css';
 
 export default function Onloadanimation() {
-  // State to track when the animation should be hidden
-  const [hideAnimation, setHideAnimation] = useState(false);
+  const [isAnimationRunning, setisAnimationRunning] = useState(false);
+  const [isAnimationVisible, setIsAnimationVisible] = useState(true);
 
   useEffect(() => {
-    // Function to handle animation opening page
-    function animationOpeningPage() {
-      setTimeout(() => {
-        // Add classes to start the animations
-        setHideAnimation(true);
-      }, 1000);
-      setTimeout(() => {
-        // Set state to hide the parent container after the animations
-        setHideAnimation(true);
-      }, 4000);
-    }
+    setTimeout(() => {
+      setisAnimationRunning(true);
+    }, 500);
 
-    // Run the animationOpeningPage function when the component is mounted
-    animationOpeningPage();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+    setTimeout(() => {
+      setIsAnimationVisible(false);
+    }, 2000);
+  }, []);
 
   return (
-    <div className={`${styles.parent_Animation_loading_page} ${hideAnimation && styles.dpnone}`}>
-      <div className={`${styles.Animation_loading_page} ${hideAnimation && styles.Animation_loading_page_add}`}>
-        <h3 className={styles.txt_Animation_loading_page}>Port</h3>
-        <h4 className={styles.txt_Animation_loading_page2}>Romain</h4>
+    <div className={`${styles.parentAnimationLoadingPage} ${isAnimationVisible ? '' : styles.dpnone}`}>
+      <div className={`${styles.AnimationLoadingPage} ${isAnimationRunning ? styles.AnimationLoadingPageAdd : ''}`}>
+        <h3 className={styles.txtAnimationLoadingPage}>Port</h3>
+        <h4 className={styles.txtAnimationLoadingPage2}>Romain</h4>
       </div>
-      <div className={`${styles.Animation_loading_page2} ${hideAnimation && styles.Animation_loading_page2_add}`}>
-        <h3 className={styles.txt_Animation_loading_page}>folio</h3>
-        <h4 className={styles.txt_Animation_loading_page2}>PARISOT</h4>
+      <div className={`${styles.AnimationLoadingPage2} ${isAnimationRunning ? styles.AnimationLoadingPage2Add : ''}`}>
+        <h3 className={styles.txtAnimationLoadingPage}>folio</h3>
+        <h4 className={styles.txtAnimationLoadingPage2}>PARISOT</h4>
       </div>
     </div>
   );
