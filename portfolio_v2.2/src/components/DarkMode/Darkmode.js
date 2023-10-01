b8b37805836f6f09da362d381dc1779d8a5002d0
unknown <romainparisot.pro@gmail.com>
2023-10-01 16:21:51 +0200

    script 2/2 sark mode

--- portfolio_v2.2/src/components/DarkMode/Darkmode.js
+++ portfolio_v2.2/src/components/DarkMode/Darkmode.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Darkmode.module.css';
 
 export default function Darkmode() {
+  // on click to activate dark mode
   function darkMode() {
     const varColor = document.querySelector(':root');
     const valuevarcolor = getComputedStyle(varColor);
-
     if (valuevarcolor.getPropertyValue('--dark-mode-off') === '#ffffff') {
       varColor.style.setProperty('--dark-mode-off', '#000000');
       varColor.style.setProperty('--dark-mode-on', '#ffffff');
@@ -14,12 +14,47 @@ export default function Darkmode() {
       varColor.style.setProperty('--dark-mode-on', '#000000');
     }
   }
+  // animation of svg dark mode
+  function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
+  }
+
+  const [SunAnim, setSunAnim] = useState(true);
+  function SvgSunAnim() {
+    setSunAnim(prevSunAnim => !prevSunAnim);
+  }
+
+  function SvgMoonAnim() {
+    const randomSideAnimation = getRandomIntInclusive(1, 3);
+    switch (randomSideAnimation) {
+      case 1:
+        return styles.darkmodeSvgSpark0;
+      case 2:
+        return styles.darkmodeSvgSparkAnim;
+      case 3:
+        return styles.darkmodeSvgSparkAnim2;
+      default:
+        return styles.darkmodeSvgSpark0;
+    }
+  }
+
+  useEffect(() => {
+    SvgMoonAnim();
+    SvgSunAnim();
+    const interval = setInterval(() => {
+      SvgMoonAnim();
+      SvgSunAnim();
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className={`${styles.divbtdarkmode}`} onClick={darkMode}>
+    <button type="button" className={`${styles.divbtdarkmode}`} onClick={darkMode}>
       <svg
         version="1.1"
-        className={`${styles.darkmode_svg}`}
+        className={`${styles.darkmodeSvg}`}
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 1000 1000"
@@ -27,43 +62,51 @@ export default function Darkmode() {
       >
         <path d="M817.1,544.8c62.48,20.85,126.89,92,119.81,187.5c-6.93,93.38-78.41,165.38-172.33,173.24c-92,7.7-165.9-53.26-188.55-119.68c74.19,22.99,141.37,11.12,196.54-43.85C827.86,686.93,840.16,619.85,817.1,544.8z" />
         <path
-          className={`${styles.darkmode_svg_spark} ${styles.darkmode_svg_spark0}`}
+          className={`${styles.darkmodeSvgSpark} ${SvgMoonAnim()}`}
           d="M564.94,609.82c44.78-22.85,44.78-22.85,76.31-75.46c13.11,36.82,37.95,61.47,76.3,75c-38.3,13.58-63.3,38.05-76.84,76.92C626.92,647.93,602.57,623.15,564.94,609.82z"
         />
         <path d="M406.24,280.99c0.09,68.82-55.82,125.07-124.5,125.25c-69.25,0.18-125.57-56.03-125.48-125.25c0.09-68.81,56.11-124.72,124.98-124.73C350.06,156.25,406.15,212.22,406.24,280.99z" />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_topleft}`}
+          className={`${styles.darkmodeSvgSun} 
+          ${SunAnim ? styles.darkmodeSvgSunTopLeftAnim : ''}`}
           d="M125.44,189.25c-9.99-29.88-20.63-61.74-32-95.77c33.47,11.12,65.43,21.74,97.08,32.25C168.65,147.09,147.06,168.16,125.44,189.25z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_topright}`}
+          className={`${styles.darkmodeSvgSun} 
+          ${SunAnim ? styles.darkmodeSvgSunTopRightAnim : ''}`}
           d="M436.3,191.69c-22.17-22.14-43.27-43.21-65.45-65.35c32.19-10.77,64.16-21.46,98.12-32.82C457.82,127,447.18,158.98,436.3,191.69z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_botleft}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunBotLeftAnim : ''}`}
           d="M126.22,370.76c22.18,22.16,43.29,43.26,65.52,65.48c-32.24,10.79-64.26,21.51-98.26,32.89C104.65,435.55,115.31,403.53,126.22,370.76z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_botright}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunBotRightAnim : ''}`}
           d="M437.08,373.18c10.11,30.33,20.67,61.97,31.91,95.69c-33.4-11.09-65.35-21.7-96.92-32.19C393.87,415.4,415.44,394.33,437.08,373.18z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_top}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunTopAnim : ''}`}
           d="M327.88,108.92c-31.54,0-61.64,0-93.21,0c15.39-30.82,30.42-60.92,46.54-93.23C297.18,47.6,312.3,77.79,327.88,108.92z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_left}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunLeftAnim : ''}`}
           d="M108.76,327.77c-31.02-15.52-61.03-30.53-92.98-46.52c31.96-15.99,61.97-31,92.98-46.51C108.76,266.04,108.76,296.08,108.76,327.77z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_right}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunRightAnim : ''}`}
           d="M453.53,327.91c0-31.73,0-61.81,0-93.32c30.94,15.45,60.99,30.45,93.36,46.62C514.88,297.23,484.58,312.39,453.53,327.91z"
         />
         <path
-          className={`${styles.darkmode_svg_sun} ${styles.darkmode_svg_sun_bot}`}
+          className={`${styles.darkmodeSvgSun}
+          ${SunAnim ? styles.darkmodeSvgSunBotAnim : ''}`}
           d="M234.51,453.36c31.81,0,61.72,0,93.44,0c-15.54,31.1-30.63,61.3-46.7,93.45C265.24,514.8,250.22,484.76,234.51,453.36z"
         />
       </svg>
-    </div>
+    </button>
   );
 }
