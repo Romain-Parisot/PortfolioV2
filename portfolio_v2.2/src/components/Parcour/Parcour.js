28806c715f8be7dd56cd5009297413d5b3e341fc
unknown <romainparisot.pro@gmail.com>
2023-10-07 12:16:23 +0200

    page style

--- portfolio_v2.2/src/components/Parcour/Parcour.js
+++ portfolio_v2.2/src/components/Parcour/Parcour.js
@@ -1,19 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import styles from './Parcour.module.css';
+import styles from './Parcour.module.css';
 import BulletPointParcour from './BulletPointParcour/BulletPointParcour';
 
 export default function Parcour() {
-  // bar parcour animation
+  // get scroll percentage
   const [, setScrollPercentage] = useState(0);
-
-  // useEffect(() => {
-  //   const updateScrollPercentage = () => {
-  //     const newScrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
-  //     setScrollPercentage(newScrollPercentage);
-  //     console.log(scrollPercentage);
-  //   };
-  //   window.addEventListener('scroll', updateScrollPercentage());
-  // }, []);
 
   useEffect(() => {
     const updateScrollPercentage = () => {
@@ -29,6 +20,8 @@ export default function Parcour() {
     };
   }, []);
 
+  // animation timeline
+
   // call bullet point component 4 times
   const bulletPoints = [];
 
@@ -37,24 +30,24 @@ export default function Parcour() {
   }
 
   return (
-    <section id="parcour">
+    <section id={styles.Parcour}>
       {bulletPoints}
-      <h2>Mon parcour :</h2>
-      <div className="divtimeline">
-        <svg id="parcourSvg">
-          <rect id="parcourBarSvg" />
-          <rect id="parcourBarSvg2" />
+      <h2 className={styles.parcourTitle}>Mon parcour :</h2>
+      <div className={styles.divtimeline}>
+        <svg id={styles.parcourSvg}>
+          <rect id={styles.parcourBarSvg} />
+          <rect id={styles.parcourBarSvg2} />
         </svg>
 
-        <div className="parcourElmt parcourElmtLeft parcourElmt1">
-          <div className="parcourElmtChild">
+        <div className={`${styles.parcourElmt} ${styles.parcourElmtLeft} ${styles.parcourElmt1}`}>
+          <div className={styles.parcourElmtChild}>
             <h3>
               Étudiant en Coding & Digital Innovation a l&apos;
               <a href="https://www.iim.fr/" target="_blank" rel="noreferrer">
                 IIM
               </a>
             </h3>
-            <div className="parcourElmtDetails">
+            <div className={styles.parcourElmtDetails}>
               <p>- Socle en Coding, Creation & Design, Communication Digital & e-Business. (1ans)</p>
               <a href="https://www.iim.fr/cursus/bachelor-premiere-annee/" target="_blank" rel="noreferrer">
                 Listes des compétences ici.
@@ -72,14 +65,14 @@ export default function Parcour() {
                 Listes des compétences ici.
               </a>
             </div>
-            <p className="parcourElmtDate">Septembre/2021 - Août/2026</p>
+            <p className={styles.parcourElmtDate}>Septembre/2021 - Août/2026</p>
           </div>
         </div>
-        <div className="parcourElmt parcourElmt_right parcourElmt2">
-          <div className="parcourElmtChild" />
+        <div className={`${styles.parcourElmt} ${styles.parcourElmt_right} ${styles.parcourElmt2}`}>
+          <div className={styles.parcourElmtChild} />
         </div>
-        <div className="parcourElmt parcourElmtLeft parcourElmt3">
-          <div className="parcourElmtChild">
+        <div className={`${styles.parcourElmt} ${styles.parcourElmtLeft} ${styles.parcourElmt3}`}>
+          <div className={styles.parcourElmtChild}>
             <h3>
               Dévellopeur chez{' '}
               <a href="https://groupe-coriance.fr/" target="_blank" rel="noreferrer">
@@ -87,17 +80,17 @@ export default function Parcour() {
               </a>{' '}
               - Stage
             </h3>
-            <div className="parcourElmtDetails">
+            <div className={styles.parcourElmtDetails}>
               <p>- Développement d&apos;application de gestion avec Power Apps.</p>
               <p>- Automatisation de process avec Power Automate.</p>
               <p>- Création de sites et bases de données sur SharePoint.</p>
               <p>- Réalisation de dashboards sur Power Bi</p>
             </div>
-            <p className="parcourElmtDate">Mai/2023 - Juillet/2023</p>
+            <p className={styles.parcourElmtDate}>Mai/2023 - Juillet/2023</p>
           </div>
         </div>
-        <div className="parcourElmt parcourElmt_right parcourElmt4">
-          <div className="parcourElmtChild">
+        <div className={`${styles.parcourElmt} ${styles.parcourElmt_right} ${styles.parcourElmt4}`}>
+          <div className={styles.parcourElmtChild}>
             <h3>
               Dévellopeur web & logiciel chez{' '}
               <a href="https://www.altusgroup.com/" target="_blank" rel="noreferrer">
@@ -105,7 +98,7 @@ export default function Parcour() {
               </a>{' '}
               - Alternance
             </h3>
-            <div className="parcourElmtDetails">
+            <div className={styles.parcourElmtDetails}>
               <p>- Développement d&apos;applications micro-frontend en React.</p>
               <p>- Développement de micro-services en Python.</p>
               <p>- Déploiement et configuration des applications sur AWS.</p>
@@ -113,7 +106,7 @@ export default function Parcour() {
                 - Développement, maintenance et déploiement de composants React pour le Design System du groupe Altus.
               </p>
             </div>
-            <p className="parcourElmtDate">Septembre/2023 - Toujours en activité</p>
+            <p className={styles.parcourElmtDate}>Septembre/2023 - Toujours en activité</p>
           </div>
         </div>
       </div>
