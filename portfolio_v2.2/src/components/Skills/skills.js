8bbbd26f38c7ae7d24c1e297f85cde4170e3116d
unknown <romainparisot.pro@gmail.com>
2023-10-08 23:51:48 +0200

    skills front

--- portfolio_v2.2/src/components/Skills/skills.js
+++ portfolio_v2.2/src/components/Skills/skills.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import styles from './skills.module.css';
 
+import ReactSvg from './frontsvg/ReactSvg';
+import VuejsSvg from './frontsvg/VuejsSvg';
+import TypescriptSvg from './frontsvg/TypescriptSvg';
+import SassSvg from './frontsvg/SassSvg';
+import TailwindSvg from './frontsvg/TailwindSvg';
+
 export default function Skills() {
-  return <section></section>;
+  return (
+    <section id={styles.skills}>
+      <h2 className={styles.titleSkillsContainer}>Front-end</h2>
+      <div className={styles.skillsContainer}>
+        <ReactSvg />
+        <VuejsSvg />
+        <TypescriptSvg />
+        <SassSvg />
+        <TailwindSvg />
+      </div>
+      <h2 className={styles.titleSkillsContainer}>Back-end</h2>
+      <div className={styles.skillsContainer}>
+        <p>just to not let empty</p>
+      </div>
+      <h2 className={styles.titleSkillsContainer}>Microsoft</h2>
+      <div className={styles.skillsContainer}>
+        <p>just to not let empty</p>
+      </div>
+      <h2 className={styles.titleSkillsContainer}>CMS</h2>
+      <div className={styles.skillsContainer}>
+        <p>just to not let empty</p>
+      </div>
+    </section>
+  );
 }
