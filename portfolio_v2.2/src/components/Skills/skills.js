0aeb71330b3f08cec90b76f0d5afaa3977ab14c5
Rowars2 <romain.parisot@edu.devinci.fr>
2023-10-10 18:32:03 +0200

    other svg

--- portfolio_v2.2/src/components/Skills/skills.js
+++ portfolio_v2.2/src/components/Skills/skills.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './skills.module.css';
 
+// front imports
 import ReactSvg from './frontsvg/ReactSvg';
 import VuejsSvg from './frontsvg/VuejsSvg';
 import TypescriptSvg from './frontsvg/TypescriptSvg';
 import SassSvg from './frontsvg/SassSvg';
 import TailwindSvg from './frontsvg/TailwindSvg';
+
+// back imports
+import PythonSvg from './backsvg/PythonSvg';
+import PhpSvg from './backsvg/PhpSvg';
+import SymphonySvg from './backsvg/SymphonySvg';
+import SqlSvg from './backsvg/SqlSvg';
+
+// microsoft imports
+
+// cms imports
+import ShopifySvg from './cmssvg/ShopifySvg';
+import WebflowSvg from './cmssvg/WebflowSvg';
+import WordpressSvg from './cmssvg/WordpressSvg';
 
 export default function Skills() {
   return (
