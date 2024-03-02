1f169ce31bd54d3c571960fc7147bdbea8be168a
unknown <romainparisot.pro@gmail.com>
2024-03-02 16:38:57 +0100

    feat: skills animation

--- PortfolioReactV2/src/components/Skills/skills.js
+++ PortfolioReactV2/src/components/Skills/skills.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import styles from './skills.module.css';
 
 // front imports
@@ -25,9 +27,33 @@ import ShopifySvg from './cmssvg/ShopifySvg';
 import WebflowSvg from './cmssvg/WebflowSvg';
 import WordpressSvg from './cmssvg/WordpressSvg';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Skills() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const skills = containerRef.current.querySelectorAll('.skill');
+
+    const animations = [
+      { x: 0, y: -100 }, // from top
+      { x: -100, y: 0 }, // from left
+      { x: 100, y: 0 }, // from right
+      { x: 0, y: 100 }, // from bottom
+    ];
+
+    skills.forEach(skill => {
+      const animation = animations[Math.floor(Math.random() * animations.length)];
+
+      gsap.fromTo(
+        skill,
+        { x: animation.x, y: animation.y, opacity: 0 },
+        { x: 0, y: 0, opacity: 1, duration: 1, scrollTrigger: skill },
+      );
+    });
+  }, []);
   return (
-    <section id={styles.skills}>
+    <section id={styles.skills} ref={containerRef}>
       <h2 className={styles.titleSkillsContainer}>Front-end</h2>
       <div className={styles.skillsContainer}>
         <a
@@ -36,7 +62,7 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <VuejsSvg />
             <h3>Vue Js</h3>
           </div>
@@ -47,25 +73,25 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <TypescriptSvg />
             <h3>TypeScript</h3>
           </div>
         </a>
         <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <ReactSvg />
             <h3>React</h3>
           </div>
         </a>
         <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <TailwindSvg />
             <h3>Tailwind</h3>
           </div>
         </a>
         <a href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <SassSvg />
             <h3>Sass</h3>
           </div>
@@ -74,25 +100,25 @@ export default function Skills() {
       <h2 className={styles.titleSkillsContainer}>Back-end</h2>
       <div className={styles.skillsContainer}>
         <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <PythonSvg />
             <h3>Python</h3>
           </div>
         </a>
         <a href="https://www.php.net/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <PhpSvg />
             <h3>Php</h3>
           </div>
         </a>
         <a href="https://symfony.com/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <SymphonySvg />
             <h3>Symphony</h3>
           </div>
         </a>
         <a href="https://www.mysql.com/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <SqlSvg />
             <h3>SQL</h3>
           </div>
@@ -106,7 +132,7 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <PowerAppSvg />
             <h3>Power App</h3>
           </div>
@@ -117,7 +143,7 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <PowerAutomateSvg />
             <h3>Power Automate</h3>
           </div>
@@ -128,7 +154,7 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <PowerBiSvg />
             <h3>Power BI</h3>
           </div>
@@ -139,7 +165,7 @@ export default function Skills() {
           rel="noopener noreferrer"
           className="MouseHoverEffect"
         >
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <SharepointSvg />
             <h3>Sharepoint</h3>
           </div>
@@ -148,19 +174,19 @@ export default function Skills() {
       <h2 className={styles.titleSkillsContainer}>CMS</h2>
       <div className={styles.skillsContainer}>
         <a href="https://wordpress.org/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <WordpressSvg />
             <h3>Wordpress</h3>
           </div>
         </a>
         <a href="https://www.shopify.fr/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <ShopifySvg />
             <h3>Shopify</h3>
           </div>
         </a>
         <a href="https://webflow.com/" target="_blank" rel="noopener noreferrer" className="MouseHoverEffect">
-          <div className={styles.cardSkills}>
+          <div className={`${styles.cardSkills} skill`}>
             <WebflowSvg />
             <h3>Webflow</h3>
           </div>
